fba811af847e93b4cd72c45bf5289581d87c288b
amati.mattia <amati.mattia@TAVE-PC04.OSDBSESTO.local>
2021-04-15 14:08:15 +0200

    commit 2

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -5,6 +5,13 @@
  */
 package javaapplication1;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  *
  * @author amati.mattia
@@ -14,8 +21,34 @@ public class JavaApplication1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+        Studente s = new Studente("mattia", "amati", "3");
         
+        FileOutputStream file = new FileOutputStream("test.ser");
+        
+        ObjectOutputStream os = new ObjectOutputStream(file);
+        
+        
+        os.writeObject((float)5.2);
+    
+        os.flush();
+        
+        os.close();
+        
+        file.flush();
+        
+        file.close();
+//        
+//        FileInputStream f = new FileInputStream("test.ser");
+//
+//        ObjectInputStream ot = new ObjectInputStream(f);
+//         
+//        ot.readObject();   
+//       
+//        
+//        int valore = (int)ot.readObject();   
+//        
+//        System.out.println(valore);
     }
     
 }
